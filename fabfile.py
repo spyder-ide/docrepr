13e4d867e724f408b5d2dd21888b2f8a28d8fbc6
Carlos Cordoba <ccordoba12@gmail.com>
2015-06-28 11:13:57 -0500

    Add a test for math

--- fabfile.py
+++ fabfile.py
@@ -10,14 +10,22 @@ import webbrowser
 
 import oinspect.sphinxify as oi
 
+def _show_page(content, fname):
+    with open(fname, 'w') as f:
+        f.write(content)
+    webbrowser.open_new_tab(fname)
+
 def test_basic():
     """Test with an empty context"""
     docstring = 'A test'
     content = oi.sphinxify(docstring, oi.generate_context())
-    page_name = '/tmp/test_basic.html'
-    with open(page_name, 'w') as f:
-        f.write(content)
-    webbrowser.open_new_tab(page_name)
+    _show_page(content, '/tmp/test_basic.html')
+
+def test_math():
+    """Test a docstring with Latex on it"""
+    docstring = 'This is a rational number :math:`\\frac{x}{y}`'
+    content = oi.sphinxify(docstring, oi.generate_context())
+    _show_page(content, '/tmp/test_math.html')
 
 def run_all():
     """Run all tests"""
