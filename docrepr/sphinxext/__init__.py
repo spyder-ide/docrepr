0197cc7c201d8709f3e9f56ba8781acd9047c3d7
martinRenou <martin.renou@gmail.com>
2021-12-09 09:54:08 +0100

    Format deprecation warning message
    Co-authored-by: CAM Gerlach <CAM.Gerlach@Gerlach.CAM>

--- docrepr/sphinxext/__init__.py
+++ docrepr/sphinxext/__init__.py
@@ -2,7 +2,7 @@ from warnings import warn
 
 warn(
     """
-    docrepr.sphinxext.plot_directive is deprecated and will eventually be removed,
+    docrepr.sphinxext.plot_directive is deprecated and will eventually be removed;
     please use matplotlib.sphinxext.plot_directive instead
     """,
     DeprecationWarning
