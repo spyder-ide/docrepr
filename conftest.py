a5dbb781941008c785c9150ef04cca39139036ef
C.A.M. Gerlach <CAM.Gerlach@Gerlach.CAM>
2021-12-15 21:49:46 -0600

    Rewrite Pytest tests to be cleaner and more idiomatic

---
+++ conftest.py
@@ -0,0 +1,35 @@
+"""Setup for Pytest."""
+
+# Standard library imports
+import webbrowser
+
+# Third party imports
+import pytest
+
+
+# ---- Constants
+
+OPEN_BROWSER_OPTION = '--open-browser'
+
+
+# ---- Pytest hooks
+
+def pytest_addoption(parser):
+    """Add an option to open the user's web browser with HTML test output."""
+    parser.addoption(
+        OPEN_BROWSER_OPTION,
+        action='store_true',
+        default=False,
+        help='For tests that generate HTML output, open it in a web browser',
+    )
+
+
+# ---- Fixtures
+
+@pytest.fixture
+def open_browser(request):
+    """Show the passed URL in the user's web browser if passed."""
+    def _open_browser(url):
+        if request.config.getoption(OPEN_BROWSER_OPTION):
+            webbrowser.open_new_tab(url)
+    return _open_browser
--- docrepr/tests/test_output.py
+++ docrepr/tests/test_output.py
@@ -1,117 +1,150 @@
 """Simple tests of docrepr's output."""
 
-# Stdlib imports
-import webbrowser
+# Standard library imports
+import copy
+import subprocess
+from pathlib import Path
 
-# 3rd party imports
+# Third party imports
+import numpy as np
+import pytest
 from IPython.core.oinspect import Inspector, object_info
 
 # Local imports
 import docrepr
-import docrepr.sphinxify as spxy
+import docrepr.sphinxify as sphinxify
 
 
-# Main inspector instance
-inspector = Inspector()
+# ---- Test data
 
-
-def _show_page(url):
-    webbrowser.open_new_tab(url)
-
-
-def test_empty_oinfo():
-    """Test with totally empty oinfo"""
-    oinfo = object_info()
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_basic():
-    """Test with an empty context"""
-    oinfo = object_info()
-    oinfo['name'] = 'Foo'
-    oinfo['argspec'] = {}
-    oinfo['docstring'] = 'A test'
-    oinfo['type_name'] = 'Function'
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_math():
-    """Test a docstring with Latex on it"""
-    oinfo = object_info()
-    oinfo['name'] = 'Foo'
-    oinfo['docstring'] = 'This is some math :math:`a^2 = b^2 + c^2`'
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_no_render_math():
-    """Test a docstring with Latex on it but without rendering it"""
-    oinfo = object_info()
-    oinfo['name'] = 'Foo'
-    oinfo['docstring'] = 'This is a rational number :math:`\\frac{x}{y}`'
-    docrepr.options['render_math'] = False
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_numpy_sin():
-    """Test for numpy.sin docstring"""
-    import numpy as np
-    oinfo = inspector.info(np.sin)
-    oinfo['name'] = 'sin'
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_collapse():
-    """Test the collapse option"""
-    import numpy as np
-    oinfo = inspector.info(np.sin)
-    oinfo['name'] = 'sin'
-    docrepr.options['collapse_sections'] = True
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_outline():
-    """Test the outline option"""
-    import numpy as np
-    oinfo = inspector.info(np.sin)
-    oinfo['name'] = 'sin'
-    docrepr.options['outline'] = True
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
-
-
-def test_plot():
-    """Test for plots"""
-    docstring = """
+LONG_DOCSTRING = """
 .. plot::
 
    >>> import matplotlib.pyplot as plt
    >>> plt.plot([1,2,3], [4,5,6])
 """
-    oinfo = object_info()
-    oinfo['name'] = 'Foo'
-    oinfo['docstring'] = docstring
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
+
+# Test cases
+TEST_CASES = {
+    'empty_oinfo': {
+        'obj': None,
+        'oinfo': {},
+        'options': {},
+        },
+    'basic': {
+        'obj': None,
+        'oinfo': {
+            'name': 'Foo',
+            'argspec': {},
+            'docstring': 'A test',
+            'type_name': 'Function',
+            },
+        'options': {},
+        },
+    'render_math': {
+        'obj': None,
+        'oinfo': {
+            'name': 'Foo',
+            'docstring': 'This is some math :math:`a^2 = b^2 + c^2`',
+            },
+        'options': {},
+        },
+    'no_render_math': {
+        'obj': None,
+        'oinfo': {
+            'name': 'Foo',
+            'docstring': 'This is a rational number :math:`\\frac{x}{y}`',
+            },
+        'options': {'render_math': False},
+        },
+    'numpy_sin': {
+        'obj': np.sin,
+        'oinfo': {'name': 'sin'},
+        'options': {},
+        },
+    'collapse': {
+        'obj': np.sin,
+        'oinfo': {'name': 'sin'},
+        'options': {'collapse_sections': True},
+        },
+    'outline': {
+        'obj': np.sin,
+        'oinfo': {'name': 'sin'},
+        'options': {'outline': True},
+        },
+    'plot': {
+        'obj': None,
+        'oinfo': {
+            'name': 'Foo',
+            'docstring': LONG_DOCSTRING
+            },
+        'options': {},
+        },
+    'python_docs': {
+        'obj': subprocess.run,
+        'oinfo': {'name': 'run'},
+        'options': {},
+        },
+    'no_docstring': {
+        'obj': None,
+        'oinfo': {'docstring': '<no docstring>'},
+        'options': {},
+        },
+    }
 
 
-def test_docs_py():
-    """Test linking to docs.python.org"""
-    import subprocess as sp
-    oinfo = inspector.info(sp.Popen)
-    oinfo['name'] = 'Popen'
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
+# ---- Helper functions
+
+def _test_cases_to_params(test_cases):
+    return [tuple(test_case.values()) for test_case in test_cases.values()]
 
 
-def test_no_doc():
-    """Test for no docstring"""
-    oinfo = object_info()
-    oinfo['docstring'] = '<no docstring>'
-    url = spxy.rich_repr(oinfo)
-    _show_page(url)
+# ---- Fixtures
+
+@pytest.fixture(name='build_oinfo')
+def fixture_build_oinfo():
+    """Generate object information for tests."""
+    def _build_oinfo(obj=None, **oinfo_data):
+        if obj is not None:
+            oinfo = Inspector().info(obj)
+        else:
+            oinfo = object_info()
+        oinfo = {**oinfo, **oinfo_data}
+        return oinfo
+    return _build_oinfo
+
+
+@pytest.fixture(name='set_docrepr_options')
+def fixture_set_docrepr_options():
+    """Set docrepr's rendering options and restore them after."""
+    default_options = copy.deepcopy(docrepr.options)
+
+    def _set_docrepr_options(**docrepr_options):
+        docrepr.options.update(docrepr_options)
+
+    yield _set_docrepr_options
+    docrepr.options.clear()
+    docrepr.options.update(default_options)
+
+
+# ---- Tests
+
+@pytest.mark.parametrize(
+    ('obj', 'oinfo_data', 'docrepr_options'),
+    _test_cases_to_params(TEST_CASES),
+    ids=list(TEST_CASES.keys()),
+    )
+def test_sphinxify(
+        build_oinfo, set_docrepr_options, open_browser,
+        obj, oinfo_data, docrepr_options,
+        ):
+    oinfo = build_oinfo(obj, **oinfo_data)
+    set_docrepr_options(**docrepr_options)
+    url = sphinxify.rich_repr(oinfo)
+    output_file = Path(url)
+    assert output_file.is_file()
+    assert output_file.suffix == '.html'
+    assert output_file.stat().st_size > 512
+    file_text = output_file.read_text(encoding='utf-8', errors='strict')
+    assert len(file_text) > 512
+    open_browser(url)
